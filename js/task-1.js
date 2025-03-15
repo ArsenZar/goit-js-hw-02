ca8d1f20f6cc2a556f89aa6aaddd93b11eaa94c5
ArsenZar <arsenzar@gmail.com>
2025-03-15 18:38:44 +0200

    test with while loop

--- js/task-1.js
+++ js/task-1.js
@@ -2,7 +2,25 @@
 
 /* 
 
+--- test with while loop
 
+let totalResoult = 0;
+
+function calculateTotal(number) {
+  let startNum = 0;
+  while (startNum < number){
+    startNum++;
+      totalResoult += startNum;
+  }
+  return totalResoult;
+}
+
+console.log(`---` + totalResoult);
+calculateTotal(18);
+console.log(totalResoult);
+.
+.
+.
 --- add both parts, fole name + extension and add dot between them.
 
 function createFileName(name, ext) {
