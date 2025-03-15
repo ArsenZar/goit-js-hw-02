f9f727f435c9ae921fcb19b1853f690e6d892460
ArsenZar <arsenzar@gmail.com>
2025-03-16 00:02:15 +0200

    use % first time

--- js/task-1.js
+++ js/task-1.js
@@ -1,18 +1,36 @@
 function sumTo(number) {
     let totalResoult = 0;
-    let i;
 
-    for (i = 0; totalResoult <= number; i += 1) {
-        totalResoult += i;
+    for (let i = 0; i <= number; i += 1) {
+        if (i % 2 == 0) {
+            totalResoult += i;
+        }
     }
-    return i;
+    return totalResoult;
 };
 
-console.log(sumTo(170));
+console.log(sumTo(7));
 
 
 
 /* 
+--- here i use first time % operator to check if the number is even and add it to the totalResoult if it true.
+
+function sumTo(number) {
+    let totalResoult = 0;
+
+    for (let i = 0; i <= number; i += 1) {
+        if (i % 2 == 0) {
+            totalResoult += i;
+        }
+    }
+    return totalResoult;
+};
+
+console.log(sumTo(7));
+.
+.
+.
 --- thats the same as the previous task but different way.
 
 function sumTo(number) {
