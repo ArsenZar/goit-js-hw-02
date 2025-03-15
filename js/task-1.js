0b78f1675cdf6acac5312cc751f0b70d036a39e4
ArsenZar <arsenzar@gmail.com>
2025-03-15 18:37:32 +0200

    crop main name of file without extension

---
+++ js/task-1.js
@@ -0,0 +1,13 @@
+
+
+/* 
+
+
+--- crop main name of file without extension.
+
+function getFileName(file) {
+  indexFile = file.indexOf(".");
+  return indexFile >= 0 ? file.slice(0, indexFile) : file;
+}
+
+*/
