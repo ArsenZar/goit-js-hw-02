cea7f9db729477d7ddb2b5d49202da6530d46902
ArsenZar <arsenzar@gmail.com>
2025-03-16 15:02:49 +0200

    task-1 is ready

--- js/task-1.js
+++ js/task-1.js
@@ -1,15 +1,17 @@
-function sumTo(number) {
-    let totalResoult = 0;
-
-    for (let i = 0; i <= number; i += 1) {
-        if (i % 2 == 0) {
-            totalResoult += i;
-        }
+function makeTransaction(quantity, pricePerDroid, customerCredits){
+    let totalPrice = quantity * pricePerDroid;
+    if (totalPrice > customerCredits) { 
+        return `Insufficient funds!`;
+    } else {
+        return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
     }
-    return totalResoult;
-};
+}
 
-console.log(sumTo(7));
+console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
+console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
+console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
 
 
 
